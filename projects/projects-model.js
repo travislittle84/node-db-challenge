f97c4e17a50b9e39edaadab8128fd1107524c8b3
Travis Little <kingtrav@gmail.com>
2019-09-01 17:20:51 -0600

    projects-model shell

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -1,5 +1,35 @@
 const db = require('../data/db-config.js')
 
 module.exports = {
+    getResources,
+    getProjects,
+    getTasks,
+    addResource,
+    addProject,
+    addTask
+
+}
+
+function getResources() {
+
+}
+
+function getProjects() {
+
+}
+
+function getTasks() {
+
+}
+
+function addResource(resource_obj) {
+
+}
+
+function addProject(project_obj) {
+
+}
+
+function addTask(project_id, task_obj) {
 
 }
